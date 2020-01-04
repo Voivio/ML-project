b5e35abbb0d98d9c5c1a512cd57d50e27a544dcb
shirley-wu <shirley0@mail.ustc.edu.cn>
2020-01-05 05:52:57 +0800

    pca

--- src/pipeline/compressor.py
+++ src/pipeline/compressor.py
@@ -1,6 +1,8 @@
 from abc import ABCMeta, abstractmethod
 import numpy as np
 
+from svd_util import randomized_svd
+
 
 class Compressor(metaclass=ABCMeta):
     @abstractmethod
@@ -18,115 +20,15 @@ class Compressor(metaclass=ABCMeta):
         raise NotImplementedError
 
 
-def svd_flip(u, v, u_based_decision=True):
-    """Sign correction to ensure deterministic output from SVD.
-
-    Adjusts the columns of u and the rows of v such that the loadings in the
-    columns in u that are largest in absolute value are always positive.
-
-    Parameters
-    ----------
-    u : ndarray
-        u and v are the output of `linalg.svd` or
-        `sklearn.utils.extmath.randomized_svd`, with matching inner dimensions
-        so one can compute `np.dot(u * s, v)`.
-
-    v : ndarray
-        u and v are the output of `linalg.svd` or
-        `sklearn.utils.extmath.randomized_svd`, with matching inner dimensions
-        so one can compute `np.dot(u * s, v)`.
-
-    u_based_decision : boolean, (default=True)
-        If True, use the columns of u as the basis for sign flipping.
-        Otherwise, use the rows of v. The choice of which variable to base the
-        decision on is generally algorithm dependent.
-
-
-    Returns
-    -------
-    u_adjusted, v_adjusted : arrays with the same dimensions as the input.
-
-    """
-    if u_based_decision:
-        # columns of u, rows of v
-        max_abs_cols = np.argmax(np.abs(u), axis=0)
-        signs = np.sign(u[max_abs_cols, range(u.shape[1])])
-        u *= signs
-        v *= signs[:, np.newaxis]
-    else:
-        # rows of v, columns of u
-        max_abs_rows = np.argmax(np.abs(v), axis=1)
-        signs = np.sign(v[range(v.shape[0]), max_abs_rows])
-        u *= signs
-        v *= signs[:, np.newaxis]
-    return u, v
-
-
 class PCACompressor(Compressor):
     # TODO: reimplement or delete when releasing code
-    def __init__(self, n_components: int):
+    def __init__(self, n_components: int, seed: int = 2333):
         self.n_components = n_components
         assert isinstance(n_components, int)
+        self.seed = seed
 
-    def fit(self, x):
-        assert len(x.shape) == 2  # (N, D) arr
-        x = x.astype(np.float)
-
-        # Handle svd_solver
-        if max(x.shape) <= 500:
-            return self._fit_full(x)
-        elif self.n_components >= 1 and self.n_components < 0.8 * min(x.shape):
-            # return self._fit_randomized(x)
-            import pdb
-            pdb.set_trace()
-            return self._fit_full(x)
-        else:
-            return self._fit_full(x)
-
-    def _fit_full(self, X):
-        """Fit the model by computing full SVD on X"""
-        self.solver = 'full'
-        n_samples, n_features = X.shape
-        n_components = self.n_components
-
-        # Center data
-        self.mean_ = np.mean(X, axis=0)
-        X -= self.mean_
-
-        U, S, V = np.linalg.svd(X, full_matrices=False)
-        # flip eigenvectors' sign to enforce deterministic output
-        U, V = svd_flip(U, V)
-
-        components_ = V
-
-        # Get variance explained by singular values
-        explained_variance_ = (S ** 2) / (n_samples - 1)
-        total_var = explained_variance_.sum()
-        explained_variance_ratio_ = explained_variance_ / total_var
-        singular_values_ = S.copy()  # Store the singular values.
-
-        # Compute noise covariance using Probabilistic PCA model
-        # The sigma2 maximum likelihood (cf. eq. 12.46)
-        if n_components < min(n_features, n_samples):
-            self.noise_variance_ = explained_variance_[n_components:].mean()
-        else:
-            self.noise_variance_ = 0.
-
-        self.n_samples_, self.n_features_ = n_samples, n_features
-        self.components_ = components_[:n_components]
-        self.n_components_ = n_components
-        self.explained_variance_ = explained_variance_[:n_components]
-        self.explained_variance_ratio_ = \
-            explained_variance_ratio_[:n_components]
-        self.singular_values_ = singular_values_[:n_components]
-
-        return U, S, V
-
-    def _fit_randomized(self, X):
-        """Fit the model by computing truncated SVD (by ARPACK or randomized)
-        on X
-        """
-        self.solver = 'randomized'
+    def fit(self, X):  # (N, D) arr
+        X = X.astype(np.float)
         n_samples, n_features = X.shape
 
         # Center data
@@ -134,14 +36,11 @@ class PCACompressor(Compressor):
         X -= self.mean_
 
         # sign flipping is done inside
-        U, S, V = randomized_svd(X, n_components=n_components,
-                                 n_iter=self.iterated_power,
-                                 flip_sign=True,
-                                 random_state=random_state)
+        U, S, V = randomized_svd(X, n_components=self.n_components, flip_sign=True)
 
         self.n_samples_, self.n_features_ = n_samples, n_features
         self.components_ = V
-        self.n_components_ = n_components
+        self.n_components_ = self.n_components
 
         # Get variance explained by singular values
         self.explained_variance_ = (S ** 2) / (n_samples - 1)
@@ -153,18 +52,17 @@ class PCACompressor(Compressor):
         if self.n_components_ < min(n_features, n_samples):
             self.noise_variance_ = (total_var.sum() -
                                     self.explained_variance_.sum())
-            self.noise_variance_ /= min(n_features, n_samples) - n_components
+            self.noise_variance_ /= min(n_features, n_samples) - self.n_components
         else:
             self.noise_variance_ = 0.
 
-        return U, S, V
-
-    def fit_transform(self, X):
-        self.fit(X)
-        return self.pca.fit_transform(X)
+        return self
 
     def transform(self, X):
-        return self.pca.transform(X)
+        if self.mean_ is not None:
+            X = X - self.mean_
+        X_transformed = np.dot(X, self.components_.T)
+        return X_transformed
 
 
 COMPRESSOR_MAP = dict(
---
+++ src/pipeline/svd_util.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+
+def randomized_range_finder(A, size, n_iter, power_iteration_normalizer='auto'):
+    # Generating normal random vectors with shape: (A.shape[1], size)
+    Q = np.random.normal(size=(A.shape[1], size))
+    if A.dtype.kind == 'f':
+        # Ensure f32 is preserved as f32
+        Q = Q.astype(A.dtype, copy=False)
+
+    # Deal with "auto" mode
+    if power_iteration_normalizer == 'auto':
+        if n_iter <= 2:
+            power_iteration_normalizer = 'none'
+        else:
+            power_iteration_normalizer = 'LU'
+
+    # Perform power iterations with Q to further 'imprint' the top
+    # singular vectors of A in Q
+    for i in range(n_iter):
+        if power_iteration_normalizer == 'none':
+            Q = np.dot(A, Q)
+            Q = np.dot(A.T, Q)
+        elif power_iteration_normalizer == 'LU':
+            Q, _ = np.linalg.lu(np.dot(A, Q), permute_l=True)
+            Q, _ = np.linalg.lu(np.dot(A.T, Q), permute_l=True)
+        elif power_iteration_normalizer == 'QR':
+            Q, _ = np.linalg.qr(np.dot(A, Q), mode='economic')
+            Q, _ = np.linalg.qr(np.dot(A.T, Q), mode='economic')
+
+    # Sample the range of A using by linear projection of Q
+    # Extract an orthonormal basis
+    Q, _ = np.linalg.qr(np.dot(A, Q), mode='economic')
+    return Q
+
+
+def svd_flip(u, v, u_based_decision=True):
+    if u_based_decision:
+        # columns of u, rows of v
+        max_abs_cols = np.argmax(np.abs(u), axis=0)
+        signs = np.sign(u[max_abs_cols, range(u.shape[1])])
+        u *= signs
+        v *= signs[:, np.newaxis]
+    else:
+        # rows of v, columns of u
+        max_abs_rows = np.argmax(np.abs(v), axis=1)
+        signs = np.sign(v[range(v.shape[0]), max_abs_rows])
+        u *= signs
+        v *= signs[:, np.newaxis]
+    return u, v
+
+
+def randomized_svd(M, n_components, n_oversamples=10, n_iter='auto',
+                   power_iteration_normalizer='auto', transpose='auto',
+                   flip_sign=True):
+    n_random = n_components + n_oversamples
+    n_samples, n_features = M.shape
+
+    if n_iter == 'auto':
+        # Checks if the number of iterations is explicitly specified
+        # Adjust n_iter. 7 was found a good compromise for PCA. See #5299
+        n_iter = 7 if n_components < .1 * min(M.shape) else 4
+
+    if transpose == 'auto':
+        transpose = n_samples < n_features
+    if transpose:
+        # this implementation is a bit faster with smaller shape[1]
+        M = M.T
+
+    Q = randomized_range_finder(M, n_random, n_iter, power_iteration_normalizer)
+
+    # project M to the (k + p) dimensional space using the basis vectors
+    B = np.dot(Q.T, M)
+
+    # compute the SVD on the thin matrix: (k + p) wide
+    Uhat, s, V = np.linalg.svd(B, full_matrices=False)
+
+    del B
+    U = np.dot(Q, Uhat)
+
+    if flip_sign:
+        if not transpose:
+            U, V = svd_flip(U, V)
+        else:
+            # In case of transpose u_based_decision=false
+            # to actually flip based on u and not v.
+            U, V = svd_flip(U, V, u_based_decision=False)
+
+    if transpose:
+        # transpose back the results according to the input convention
+        return V[:n_components, :].T, s[:n_components], U[:, :n_components].T
+    else:
+        return U[:, :n_components], s[:n_components], V[:n_components, :]
