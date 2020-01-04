62982ba403c20e42bbb0c45070a21fe1a1a70297
Voivio <43064456+Voivio@users.noreply.github.com>
2020-01-04 15:21:24 -0600

    API implemenation

--- src/pipeline/landmark_detector.py
+++ src/pipeline/landmark_detector.py
@@ -1,15 +1,36 @@
 import numpy as np
+import cv2
+import dlib
 from typing import Optional
 
 
 class OpenCVDetector:
+    def __init__(self, detector_path):
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(detector_path + "shape_predictor_68_face_landmarks.dat")
+    
     def detect(self, img: np.ndarray) -> Optional[np.ndarray]:
         """
         :param img: np.ndarray, shape is (h, w, 3), BGR image
         :return: (68, 2) points, or None
         """
-        raise NotImplementedError
+#         raise NotImplementedError
+        faces_in_image = self.detector(img, 0)
+        try:
+            face = faces_in_image[0]
+         except IndexError as e:
+            return None
+
+        landmarks = self.predictor(img_gray, face)
+
+        landmarks_list = []
+        for i in range(0, landmarks.num_parts):
+            landmarks_list.append((landmarks.part(i).x, landmarks.part(i).y))
+
+        return landmarks_list
 
 
-def get_detector():
-    raise NotImplementedError
+def get_detector(detector_path):
+#     raise NotImplementedError
+    return OpenCVDetector(detector_path)
+    
